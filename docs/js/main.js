7934a99bbe7e4f0b97558084726d2d6ed4dfbee3
georapbox <georapbox@gmail.com>
2023-01-26 10:17:10 +0200

    Update demo

--- docs/js/main.js
+++ docs/js/main.js
@@ -28,19 +28,17 @@ import(componentUrl).then(res => {
     const title = document.createElement('h4');
     const ul = document.createElement('ul');
 
+    container.classList.add(status);
     title.textContent = status === 'accepted' ? 'Accepted files' : 'Rejected files';
 
     const items = files.map(item => {
       const file = status === 'accepted' ? item : item.file;
 
       return /* html */`
-          <li>
-            <div>
-              <code>
-                ${file.name} <span style="color: var(--text-main);">- ${file.size} bytes</span>
-              </code>
-            </div>
-          </li>`;
+        <li>
+          <code>${file.name}</code><code style="color: var(--text-main);">- ${file.size} bytes</code>
+        </li>
+      `;
     }).join('');
 
     container.appendChild(title);
@@ -83,20 +81,23 @@ import(componentUrl).then(res => {
     droppedFilesList.replaceChildren();
 
     droppedFilesList.innerHTML = /* html */`
-      <blockquote>
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-info-lg" viewBox="0 0 16 16">
-          <path d="m9.708 6.075-3.024.379-.108.502.595.108c.387.093.464.232.38.619l-.975 4.577c-.255 1.183.14 1.74 1.067 1.74.72 0 1.554-.332 1.933-.789l.116-.549c-.263.232-.65.325-.905.325-.363 0-.494-.255-.402-.704l1.323-6.208Zm.091-2.755a1.32 1.32 0 1 1-2.64 0 1.32 1.32 0 0 1 2.64 0Z"/>
-        </svg>
-        <em>Open the browser's console to watch emitted events with more info of the dropped files.</em>
-      </blockquote>
+      <details open>
+        <summary>Dropped files</summary>
+        <blockquote>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-info-lg" viewBox="0 0 16 16">
+            <path d="m9.708 6.075-3.024.379-.108.502.595.108c.387.093.464.232.38.619l-.975 4.577c-.255 1.183.14 1.74 1.067 1.74.72 0 1.554-.332 1.933-.789l.116-.549c-.263.232-.65.325-.905.325-.363 0-.494-.255-.402-.704l1.323-6.208Zm.091-2.755a1.32 1.32 0 1 1-2.64 0 1.32 1.32 0 0 1 2.64 0Z"/>
+          </svg>
+          <em>Open the browser's console to watch emitted events with more info of the dropped files.</em>
+        </blockquote>
+      </details>
     `;
 
     if (acceptedFilesList) {
-      droppedFilesList.appendChild(acceptedFilesList);
+      droppedFilesList.querySelector('details').appendChild(acceptedFilesList);
     }
 
     if (rejectedFilesList) {
-      droppedFilesList.appendChild(rejectedFilesList);
+      droppedFilesList.querySelector('details').appendChild(rejectedFilesList);
     }
   });
 }).catch(err => {
